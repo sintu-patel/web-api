b58b925cac931384122fcf1cf8691bfa2f126d3e
Sintu Singh <ssi139@sapient.com>
2017-05-31 11:29:57 +0530

    'file-upload-function'

--- api/controllers/cmsController.js
+++ api/controllers/cmsController.js
@@ -1,12 +1,13 @@
 'use strict';
 var mongoose = require('mongoose');
 var cmsContent = require('../models/cmsModel');
-var multer = require('multer');
+var formidable = require('formidable');
+var fs = require('fs');
 
 exports.readData = function(req, res) {
   cmsContent.find({}, function(err, data) {
     if (err) {
-      console.log('Error');
+      res.json({status: 'error while getting data'});
     }
     var tab = {
       "linksHeading": "All information from api",
@@ -26,32 +27,28 @@ exports.saveData = function(req, res) {
   });
   cmsData.save(function(err) {
     if ( err ) { 
-      console.log(err);
+      res.json({status: 'data-not-saved'});
     }
-    res.json({status: 'completed'});
+    res.json({status: 'data-saved'});
   });
 };
 
-var storage = multer.diskStorage({ //multers disk storage settings
-    destination: function (req, file, cb) {
-        cb(null, './uploads/');
-    },
-    filename: function (req, file, cb) {
-        var datetimestamp = Date.now();
-        cb(null, file.fieldname + '-' + datetimestamp + '.' + file.originalname.split('.')[file.originalname.split('.').length -1]);
-    }
-});
-
-var upload = multer({ //multer settings
-    storage: storage
-}).single('file');
-
 exports.uploadCMS = function(req, res) {
-  upload(req, res, function(err){
-      if(err){
-           res.json({error_code:1,err_desc:err});
-           return;
-      }
-      res.json({error_code:0,err_desc:null});
-  });
+  var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+      var fileData = files.uploads;
+      var fileName = fileData.name;
+      var filePath = fileData.path;
+      var uploadedFilePath = filePath;
+      var fileUploadPath = 'uploads\\' + fileName;
+      var source = fs.createReadStream(uploadedFilePath);
+      var dest = fs.createWriteStream(fileUploadPath);
+      source.pipe(dest);
+      source.on('end', function() {
+        res.json({'status': 'uploaded'});
+      });
+      source.on('error', function() {
+        res.json({'status': 'not-uploaded'});
+      });
+    });
 }
