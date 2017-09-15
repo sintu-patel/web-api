a9cf2f496317a8a1af797898dbf785acba8271a7
Sintu Singh <ssi139@sapient.com>
2017-09-15 15:59:58 +0530

    'db-config'

--- server.js
+++ server.js
@@ -4,6 +4,17 @@ var express = require('express'),
   mongoose = require('mongoose'),
   bodyParser = require('body-parser');
 
+var assert = require('assert');
+var fs = require('fs');
+var ca = [ fs.readFileSync(__dirname + "/servercert.crt") ];
+var options = {
+    mongos: {
+      ssl: true,
+      sslValidate: true,
+      sslCA: ca
+    }
+}  
+
 var apiConfig = require('./api-config');
 
 app.use(function(req, res, next) {
@@ -13,13 +24,24 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Credentials", true);
     next();
 });
-  
-mongoose.Promise = global.Promise;
-mongoose.connect(apiConfig.dbPath, function(error) {
-    if (error) {
-        console.log('db connect error');
-    }
+
+// If the connection throws an error
+mongoose.connection.on('error',function (err) {
+  console.log('Mongoose default connection error: ' + err);
 });
+mongoose.connection.on('open', function (err) {
+    assert.equal(null, err);
+    mongoose.connection.db.listCollections().toArray(function(err, collections) {
+        assert.equal(null, err);
+        collections.forEach(function(collection) {
+            console.log(collection);
+        });
+        mongoose.connection.db.close();
+        process.exit(0);
+    })
+});
+// Let's open that connection
+mongoose.connect(process.env.MONGODB_URL, options);
 
 var db = mongoose.connection;
  
