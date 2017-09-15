676cc15dd501f7b55e2866d5f834a35aaa85e0e4
Sintu Singh <ssi139@sapient.com>
2017-09-15 15:50:06 +0530

    'db-config'

--- api-config.js
+++ api-config.js
@@ -1,4 +1,6 @@
 var userDomain = process.env.USERDOMAIN;
+var MONGODB_URL_TEST= 'mongodb://admin:JHLHGBETTDJGQHNS@sl-us-south-1-portal.8.dblayer.com:22286,sl-us-south-1-portal.6.dblayer.com:22286/admin?ssl=true';
+var MONGODB_URL = 'mongodb://admin:JHLHGBETTDJGQHNS@sl-us-south-1-portal.8.dblayer.com:22286/CMS';
 var env = 'PROD';
 if (userDomain === 'SAPIENT') {
 	env = 'DEV';
@@ -7,7 +9,7 @@ var apiConfig = {
   dbPath: 'mongodb://localhost/CMS'
 }
 if (env === 'PROD') {
-	apiConfig.dbPath = 'mongodb://admin:JHLHGBETTDJGQHNS@sl-us-south-1-portal.8.dblayer.com:22286,sl-us-south-1-portal.6.dblayer.com:22286/admin?ssl=true';
+	apiConfig.dbPath = MONGODB_URL;
 }
 
 module.exports = apiConfig;
