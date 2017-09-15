8f9ca1940ef9200b7089e440b4c7c3bfc1e93977
Sintu Singh <ssi139@sapient.com>
2017-09-15 15:26:21 +0530

    'db-config'

--- api-config.js
+++ api-config.js
@@ -1,5 +1,13 @@
+var userDomain = process.env.USERDOMAIN;
+var env = 'PROD';
+if (userDomain === 'SAPIENT') {
+	env = 'DEV';
+}
 var apiConfig = {
   dbPath: 'mongodb://localhost/CMS'
+}
+if (env === 'PROD') {
+	apiConfig.dbPath = 'mongodb://10.203.101.151/CMS';
 }
 
 module.exports = apiConfig;
