429b145a4aaaf98e7c48a36c0acef40b48f02998
ssingh172 <ssingh172@sapient.com>
2019-01-04 18:39:49 -0500

    Update port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express'),
   app = express(),
-  port = process.env.PORT || 8080,
+  port = process.env.PORT || 3100,
   mongoose = require('mongoose'),
   bodyParser = require('body-parser');
 
