66fda9f1eb2bb079b0361099e43d6ea3fa47fea3
ssingh172 <ssingh172@sapient.com>
2019-01-14 13:31:19 -0500

    Add registration

--- api/jwt/auth/AuthController.js
+++ api/jwt/auth/AuthController.js
@@ -15,12 +15,12 @@ var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
 var bcrypt = require('bcryptjs');
 var config = require('../config'); // get config file
 
-router.post('/login', function(req, res) {
+router.post('/login', function (req, res) {
 
   User.findOne({ email: req.body.email }, function (err, user) {
     if (err) return res.status(500).send('Error on the server.');
     if (!user) return res.status(404).send('No user found.');
-    
+
     // check if the password is valid
     var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
     if (!passwordIsValid) return res.status(401).send({ auth: false, token: null });
@@ -35,44 +35,51 @@ router.post('/login', function(req, res) {
     res.set({
       'x-access-token': token
     })
-    res.status(200).send({ auth: true, 'x-access-token': token  });
+    res.status(200).send({ auth: true, 'x-access-token': token, name: user.name });
   });
 
 });
 
-router.get('/logout', function(req, res) {
-  res.status(200).send({ auth: false, token: null });
+router.post('/logout', function (req, res) {
+  res.status(200).send({ auth: false, 'x-access-token': null });
 });
 
-router.post('/register', function(req, res) {
+router.post('/register', function (req, res) {
 
-  var hashedPassword = bcrypt.hashSync(req.body.password, 8);
+  User.find({email: req.body.email}, function (err, user) {
+    if (err) {
+      res.status(500).send({ error: 'user could not be registered' });
+    } else if (!user || !user.length) {
+      var hashedPassword = bcrypt.hashSync(req.body.password, 8);
+      User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword
+      },
+        function (err, user) {
+          if (err) return res.status(500).send("There was a problem registering the user`.");
 
-  User.create({
-    name : req.body.name,
-    email : req.body.email,
-    password : hashedPassword
-  }, 
-  function (err, user) {
-    if (err) return res.status(500).send("There was a problem registering the user`.");
+          // if user is registered without errors
+          // create a token
+          var token = jwt.sign({ id: user._id }, config.secret, {
+            expiresIn: 86400 // expires in 24 hours
+          });
 
-    // if user is registered without errors
-    // create a token
-    var token = jwt.sign({ id: user._id }, config.secret, {
-      expiresIn: 86400 // expires in 24 hours
-    });
-
-    res.status(200).send({ auth: true, token: token });
+          res.status(200).send({ auth: true, 'x-access-token': token, name: req.body.name});
+        });
+    } else {
+      res.status(500).send({ error: 'user could not be registered' });
+    }
   });
 
 });
 
-router.post('/check-login', VerifyToken, function(req, res, next) {
+router.post('/check-login', VerifyToken, function (req, res, next) {
 
   User.findById(req.userId, { password: 0 }, function (err, user) {
-    if (err) return res.status(500).send({auth: false});
-    if (!user) return res.status(404).send({auth: false});
-    res.status(200).send({auth: true});
+    if (err) return res.status(500).send({ auth: false });
+    if (!user) return res.status(404).send({ auth: false });
+    res.status(200).send({ auth: true, name: user.name });
   });
 
 });
